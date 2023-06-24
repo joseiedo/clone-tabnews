5c2576d6f26702c6223b463074a0bdec25f10ed5
José <98621042+joseiedo@users.noreply.github.com>
2023-06-24 19:08:47 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>O medo é como o fogo, ele queima você por dentro. Se você controlá-lo, Tommy, vai queimar você. Mas se ele o controlar, vai queimar você e tudo ao seu redor. - Rocky Balboa</h1>
+  return (
+    <h1>
+      O medo é como o fogo, ele queima você por dentro. Se você controlá-lo,
+      Tommy, vai queimar você. Mas se ele o controlar, vai queimar você e tudo
+      ao seu redor. - Rocky Balboa
+    </h1>
+  );
 }
 
 export default Home;
